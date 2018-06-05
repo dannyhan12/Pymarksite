dad5873762888ef7ad63f2f97b579ce909fb560b
programmerdays <emaildhan@gmail.com>
2018-06-04 20:45:11 -0400

    Clean slug urls. Lint fix

--- app/BlogPosts.py
+++ app/BlogPosts.py
@@ -1,16 +1,19 @@
 import os
 from markdown import markdown
 
+
 class PostHeader():
     def __init__(self, title, slug):
         self.title = title
         self.slug = slug
 
+
 def getPostHeaders(page=0):
     ''' Get information about each post in the content directory.'''
     headers = []
     for f in os.listdir('content'):
-        if not f.endswith('.md'): continue
+        if not f.endswith('.md'):
+            continue
 
         firstMetaFound = False
         metaData = {}
@@ -24,15 +27,17 @@ def getPostHeaders(page=0):
                     break
                 items = line[:-1].split(':')
                 if len(items) > 1:
-                    metaData[items[0]] = items[1]
+                    metaData[items[0].strip()] = items[1].strip()
         headers.append(metaData)
     return headers
+
 
 def getPostContent(slug):
     '''Get the text for a post that matches the specified slug'''
     txt = ''
     for f in os.listdir('content'):
-        if not f.endswith('.md'): continue
+        if not f.endswith('.md'):
+            continue
 
         firstMetaFound = False
         endMetaFound = False
@@ -52,7 +57,8 @@ def getPostContent(slug):
                     txt += line
                 else:
                     items = line[:-1].split(':')
-                    if len(items) > 1 and items[0] == "Slug" and items[1] == slug:
+                    if len(items) > 1 and items[0].strip() == "Slug" and \
+                            items[1].strip() == slug:
                         slugMatch = True
 
     return markdown(txt)
--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,12 @@ from app import app
 from markdown import markdown
 from app import BlogPosts
 
+
 @app.route('/')
 @app.route('/index')
 @app.route('/blog')
 def blog():
-    md =  "# Welcome\n"
+    md = "# Welcome\n"
     md += " {I am a programmer: this is how I spend my days}\n\n"
     md += "# Blog posts\n"
     posts = BlogPosts.getPostHeaders(page=0)
@@ -15,9 +16,10 @@ def blog():
         md += '[' + p['Title'] + '](blog/' + p['Slug'] + ')'
     return markdown(md)
 
+
 @app.route('/blog/<slug>')
 def post(slug):
 
     post = BlogPosts.getPostContent(slug)
-    
+
     return post
