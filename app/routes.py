d841bf72482ab2935de931c5f1532fb13a820ff1
programmerdays <emaildhan@gmail.com>
2018-06-06 21:47:21 -0400

    Use flask bootstrap

--- app/routes.py
+++ app/routes.py
@@ -7,20 +7,25 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/index')
+def index():
+    posts = BlogPosts.getPostHeaders(page=0)
+    return render_template('blog.html', posts=posts, active_page='index')
+
 @app.route('/blog')
 def blog():
     posts = BlogPosts.getPostHeaders(page=0)
-    return render_template('blog.html', posts=posts)
-
+    return render_template('blog.html', posts=posts, active_page='blog')
 
 @app.route('/blog/<slug>')
 def post(slug):
     post = BlogPosts.getPostContent(slug)
-    return render_template('post.html', post=post)
-
+    return render_template('post.html', post=post, active_page='blog')
 
 @app.route('/projects')
-@app.route('/about')
-def wip():
-    return render_template('post.html', post="Work in progress ...")
+def projects():
+    return render_template('post.html', post="Work in progress ...", active_page='projects')
 
+
+@app.route('/about')
+def about():
+    return render_template('post.html', post="Work in progress ...", active_page='about')
